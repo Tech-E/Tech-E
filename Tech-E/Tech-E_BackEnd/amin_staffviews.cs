24717ff2d7dc409ba45e3d1ec819994e7f715a5f
shiran888 <qideeic123>
2016-02-16 15:45:36 +0000

    view staff information

---
+++ Tech-E/Tech-E_BackEnd/amin_staffviews.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tech_E_BackEnd
+{
+    public partial class amin_staffviews : Form
+    {
+        public amin_staffviews()
+        {
+            InitializeComponent();
+        }
+    }
+}
