2cf064fdfcaa3d1d088983c798a46b90d710d990
topdog5000 <p1318601x@myemail.dmu.ac.uk>
2016-02-16 16:12:30 +0000

    menu

--- Tech-E/Tech-E_BackEnd/Menu.cs
+++ Tech-E/Tech-E_BackEnd/Menu.cs
@@ -16,15 +16,5 @@ namespace Tech_E_BackEnd
         {
             InitializeComponent();
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
