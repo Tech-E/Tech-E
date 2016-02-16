46154bacc2e2b11f85d0a2a9be65d324ebe2b3ca
tiyembatapiwa <p13204566@myemail.dmu.ac.ul>
2016-02-16 15:47:46 +0000

    paymentlistform

---
+++ Tech-E/Tech-E_BackEnd/staff_paymentlist.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Tech_E_BackEnd
+{
+    partial class staff_paymentlist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstPayments = new System.Windows.Forms.ListBox();
+            this.btnEditPayment = new System.Windows.Forms.Button();
+            this.btnDeletePayment = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tech-E Staff";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "List of Payments";
+            // 
+            // lstPayments
+            // 
+            this.lstPayments.FormattingEnabled = true;
+            this.lstPayments.Location = new System.Drawing.Point(231, 108);
+            this.lstPayments.Name = "lstPayments";
+            this.lstPayments.Size = new System.Drawing.Size(725, 368);
+            this.lstPayments.TabIndex = 13;
+            // 
+            // btnEditPayment
+            // 
+            this.btnEditPayment.Location = new System.Drawing.Point(279, 492);
+            this.btnEditPayment.Name = "btnEditPayment";
+            this.btnEditPayment.Size = new System.Drawing.Size(148, 23);
+            this.btnEditPayment.TabIndex = 14;
+            this.btnEditPayment.Text = "Edit Payment";
+            this.btnEditPayment.UseVisualStyleBackColor = true;
+            this.btnEditPayment.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDeletePayment
+            // 
+            this.btnDeletePayment.Location = new System.Drawing.Point(726, 492);
+            this.btnDeletePayment.Name = "btnDeletePayment";
+            this.btnDeletePayment.Size = new System.Drawing.Size(148, 23);
+            this.btnDeletePayment.TabIndex = 15;
+            this.btnDeletePayment.Text = "Delete Payment";
+            this.btnDeletePayment.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(981, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Search By Payment No";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(984, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // staff_paymentlist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 637);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDeletePayment);
+            this.Controls.Add(this.btnEditPayment);
+            this.Controls.Add(this.lstPayments);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Name = "staff_paymentlist";
+            this.Text = "staff_paymentlist";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstPayments;
+        private System.Windows.Forms.Button btnEditPayment;
+        private System.Windows.Forms.Button btnDeletePayment;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
