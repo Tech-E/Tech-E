8996022901b02f4ac01d0a3f02e40dfb72ec4f1d
shiran888 <qideeic123>
2016-02-23 16:25:53 +0000

    tststaff

---
+++ Tech-E/Tech-E_UnitTestProject/tstStaffs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tech_E_UnitTestProject
+{
+    [TestClass]
+    public class tstStaffs
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
