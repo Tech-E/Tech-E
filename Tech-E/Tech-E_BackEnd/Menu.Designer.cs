c522258903ec708c0f54dc2a244ff92c185caf09
topdog5000 <p1318601x@myemail.dmu.ac.uk>
2016-02-23 15:36:33 +0000

    commit

--- Tech-E/Tech-E_BackEnd/Menu.Designer.cs
+++ Tech-E/Tech-E_BackEnd/Menu.Designer.cs
@@ -32,7 +32,8 @@
             this.btnProducts = new System.Windows.Forms.Button();
             this.btnAppointments = new System.Windows.Forms.Button();
             this.btnCustomers = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnStaff = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnPayments
@@ -73,21 +74,33 @@
             this.btnCustomers.UseVisualStyleBackColor = true;
             this.btnCustomers.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label1
+            // lblTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(88, 32);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(260, 25);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Tech-E Admin Main Menu";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // btnStaff
+            // 
+            this.btnStaff.Location = new System.Drawing.Point(182, 133);
+            this.btnStaff.Name = "btnStaff";
+            this.btnStaff.Size = new System.Drawing.Size(75, 23);
+            this.btnStaff.TabIndex = 5;
+            this.btnStaff.Text = "Staff";
+            this.btnStaff.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 367);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(454, 254);
+            this.Controls.Add(this.btnStaff);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnCustomers);
             this.Controls.Add(this.btnAppointments);
             this.Controls.Add(this.btnProducts);
@@ -105,6 +118,7 @@
         private System.Windows.Forms.Button btnProducts;
         private System.Windows.Forms.Button btnAppointments;
         private System.Windows.Forms.Button btnCustomers;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnStaff;
     }
 }
