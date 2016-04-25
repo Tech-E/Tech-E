ae0f5c81489682d8719f336b8a2e8a0540b96d99
topdog5000 <p1318601x@myemail.dmu.ac.uk>
2016-04-25 13:12:13 +0100

    commit

--- Tech-E/Tech-E/CustomerBackEnd.aspx.cs
+++ Tech-E/Tech-E/CustomerBackEnd.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Tech_E_ClassLibrary;
 
 namespace Tech_E
 {
@@ -11,6 +12,23 @@ namespace Tech_E
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(IsPostBack == false)
+            {
+                DisplayCustomers();
+            }
+        }
+
+        void DisplayCustomers()
+        {
+            clsCustomerCollection Customers = new clsCustomerCollection();
+
+            lstCustomer.DataSource = Customers.CustomerList;
+
+            lstCustomer.DataValueField = "AddressNo";
+
+            lstCustomer.DataTextField = "FirstName";
+
+            lstCustomer.DataBind();
 
         }
     }
