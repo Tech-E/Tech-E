d0b68f6fc4f355f000e015bfbd3cf3ed8a8ac948
JaySloan <p13203114@myemail.dmu.ac.uk>
2016-04-25 03:25:01 +0100

    new commits

---
+++ Tech-E/Tech-E_ClassLibrary/clsProductCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tech_E_ClassLibrary
+{
+    public class clsProductCollection
+    {
+        
+        public List<Tech_E_UnitTestProject.clsProduct> ProductList { get; set; }
+
+        public int Count { get; set; }
+
+        public Tech_E_UnitTestProject.clsProduct ThisProduct { get; set; }
+    }
+}
