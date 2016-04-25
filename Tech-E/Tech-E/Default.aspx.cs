c2a85376e4b3abea7d5fadda40056be54f96562e
JaySloan <p13203114@myemail.dmu.ac.uk>
2016-04-25 12:21:32 +0100

    database

---
+++ Tech-E/Tech-E/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tech_E
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //this function handles the load event for the page
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if this is the first time the page is displayed
+            if (IsPostBack == false)
+            {
+                //update the list box
+                Display
+            }
+        }
+    }
+}
