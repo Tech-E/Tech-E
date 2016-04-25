ae339b4a7656b2161e13d5ffd01cebd3379aa05d
shiran888 <qideeic123>
2016-04-25 15:08:27 +0100

    12

--- Tech-E/Tech-E/StaffDeletePage.aspx.cs
+++ Tech-E/Tech-E/StaffDeletePage.aspx.cs
@@ -16,17 +16,17 @@ namespace Tech_E
         {
             staffid = Convert.ToInt32(Session["staffid"]);
             //create an instane of the collection
-            clsStaffCollection clsstaffcollection = new clsStaffCollection();
-
-            clsstaffcollection.Delete(staffid);
-            Response.Redirect("Staffs.aspx");
+            
 
 
         }
 
         protected void btn1_Click(object sender, EventArgs e)
         {
+            clsStaffCollection clsstaffcollection = new clsStaffCollection();
 
+            clsstaffcollection.Delete(staffid);
+            Response.Redirect("Staffs.aspx");
         }
 
         protected void btn2_Click(object sender, EventArgs e)
